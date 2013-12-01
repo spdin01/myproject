27ca7f2121ed34ed5401d9e77a8d600c6e36ba75
spdin <saripudin2@gmail.com>
2013-12-01 15:07:49 +0700

    add main.cpp on tutorial2 branch

---
+++ tutorial2/main.cpp
@@ -0,0 +1,22 @@
+// Mudah & Cepat Menjadi MASTER C++
+// Oleh:  Budi Raharjo
+// Email: mbraharjo@gmail.com
+// Blog:  http://mbraharjo.blogspot.com
+
+#include <iostream.h>
+
+int main() {
+  int X;
+
+  // Menampilkan teks untuk informasi
+  cout<<"Masukkan sebuah bilangan bulat : "; 
+
+  // Membaca nilai dari keyboard dan menyimpannya 
+  // ke dalam variabel X
+  cin>>X;
+
+  // Menampilkan nilai yang telah dimasukkan
+  cout<<"Bilangan yang telah Anda masukkan adalah "<<X;
+
+  return 0;
+}
